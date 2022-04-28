a01ca49dbb04e4b4d0cb97a750353f91db90ce7a
elwiiman <emmanuel.sanro@gmail.com>
2022-04-27 23:35:18 -0500

    deleting comments

--- src/config/font/index.js
+++ src/config/font/index.js
@@ -5,47 +5,17 @@ export const fontTypes = {
                   font-weight: ${weights[0]};
                   line-height: ${lineHeights[2]}px;`,
 
-  //   paragraphBold: `font-size: ${sizes[2]};
-  //                     font-weight: ${weights[1]};
-  //                     line-height: ${lineHeights[2]}px;`,
-
-  //   paragraphHyperlink: `font-size: ${sizes[2]};
-  //                          font-weight:  ${weights[1]};
-  //                          line-height: ${lineHeights[2]}px;
-  //                          text-decoration-line: underline;`,
-
   caption: `font-size:  ${sizes[1]};
                 font-weight:  ${weights[0]};
                 line-height: ${lineHeights[1]}px;`,
 
-  //   captionBold: `font-size: ${sizes[1]};
-  //                   font-weight: ${weights[1]};
-  //                   line-height: ${lineHeights[1]}px;`,
-
-  //   micro: `font-size: ${sizes[0]};
-  //             font-weight: ${weights[0]};
-  //             line-height:  ${lineHeights[0]}px;`,
-
-  //   microBold: `font-size: ${sizes[0]};
-  //                 font-weight: ${weights[1]};
-  //                 line-height: ${lineHeights[0]}px;`,
-
   titleDesktop: `font-size: ${sizes[7]};
                    font-weight: ${weights[1]};
                    line-height: ${lineHeights[8]}px;`,
-
-  //   subtitleDesktop: `font-size: ${sizes[7]};
-  //                         font-weight: ${weights[1]};
-  //                         line-height: ${lineHeights[7]}px;
-  //                      `,
-
-  //   subtitle2Desktop: ` font-size: ${sizes[5]};
-  //                         font-weight: ${weights[1]};
-  //                         line-height: ${lineHeights[5]}px;`,
 };
 
 export const GlobalFonts = createGlobalStyle`
   body{
-    font-family: "Roboto", sans-serif;
+    font-family: "Roboto", sans-serif !important;
 }
 `;
